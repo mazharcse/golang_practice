7e56f47dd5f3faf18b312ce6676d33ef2353f1d3
MD Mazharul Islam Khan <mazhar_cse@yahoo.com>
2025-04-06 23:16:08 +0600

    Multiplexing Request Handlers

--- httpserver/main.go
+++ httpserver/main.go
@@ -19,10 +19,14 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", getRoot)
-	http.HandleFunc("/hello", getHello)
+	// http.HandleFunc("/", getRoot)
+	// http.HandleFunc("/hello", getHello)
 
-	err := http.ListenAndServe(":3333", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":3333", mux)
 
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Println("Server closed")
