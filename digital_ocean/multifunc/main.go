80f79105e8da6164d98daa4c6bd52ba2ded9efca
MD Mazharul Islam Khan <mazhar_cse@yahoo.com>
2025-04-08 21:17:09 +0600

    go routines with channel

--- digital_ocean/multifunc/main.go
+++ digital_ocean/multifunc/main.go
@@ -5,19 +5,23 @@ import (
 	"sync"
 )
 
-func generateNumbers(total int, wg *sync.WaitGroup) {
+func generateNumbers(total int, ch chan<- int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for idx := 1; idx <= total; idx++ {
 		fmt.Printf("Generating number %d\n", idx)
+		ch <- idx
 	}
 }
 
-func printNumbers(total int, wg *sync.WaitGroup) {
+func printNumbers(ch <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	for idx := 1; idx <= total; idx++ {
-		fmt.Printf("Printing number %d\n", idx)
+	// for idx := 1; idx <= total; idx++ {
+	// 	fmt.Printf("Printing number %d\n", idx)
+	// }
+	for num := range ch {
+		fmt.Printf("read %d from channel\n", num)
 	}
 }
 
@@ -26,8 +30,15 @@ func main() {
 
 	wg.Add(2)
 	total := 100
-	go printNumbers(total, &wg)
-	go generateNumbers(total, &wg)
+
+	intChan := make(chan int)
+
+	go printNumbers(intChan, &wg)
+	generateNumbers(total, intChan, &wg)
+
+	//wg.Wait()
+	
+	close(intChan)
 
 	fmt.Println("Waiting for goroutines to finish...")
 	wg.Wait()
