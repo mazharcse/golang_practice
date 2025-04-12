98657ef0e0982aaa686db91c4bb7c91f053561ad
MD Mazharul Islam Khan <mazhar_cse@yahoo.com>
2025-04-12 11:35:15 +0600

    Update main.go

--- digital_ocean/multifunc/main.go
+++ digital_ocean/multifunc/main.go
@@ -6,41 +6,35 @@ import (
 )
 
 func generateNumbers(total int, ch chan<- int, wg *sync.WaitGroup) {
-	defer wg.Done()
-
 	for idx := 1; idx <= total; idx++ {
-		fmt.Printf("Generating number %d\n", idx)
+		fmt.Printf("sending %d to channel\n", idx)
 		ch <- idx
 	}
 }
 
-func printNumbers(ch <-chan int, wg *sync.WaitGroup) {
+func printNumbers(idx int, ch <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	// for idx := 1; idx <= total; idx++ {
-	// 	fmt.Printf("Printing number %d\n", idx)
-	// }
 	for num := range ch {
-		fmt.Printf("read %d from channel\n", num)
+		fmt.Printf("%d: read %d from channel\n", idx, num)
 	}
 }
 
 func main() {
 	var wg sync.WaitGroup
+	numberChan := make(chan int)
 
-	wg.Add(2)
-	total := 100
+	for idx := 1; idx <= 3; idx++ {
+		wg.Add(1)
+		go printNumbers(idx, numberChan, &wg)
+	}
 
-	intChan := make(chan int)
+	generateNumbers(5, numberChan, &wg)
 
-	go printNumbers(intChan, &wg)
-	generateNumbers(total, intChan, &wg)
-
-	//wg.Wait()
-	
-	close(intChan)
+	close(numberChan)
 
 	fmt.Println("Waiting for goroutines to finish...")
 	wg.Wait()
 	fmt.Println("Done!")
 }
+
