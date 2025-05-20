e24cfbdae6694129d25c96cd250f56b8990f4654
MD Mazharul Islam Khan <mazhar_cse@yahoo.com>
2025-05-20 15:45:08 +0600

    Step by step web service

---
+++ GoCloudDrive/go.mod
@@ -0,0 +1,3 @@
+module go-cloud-drive
+
+go 1.22.2
---
+++ GoCloudDrive/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"		
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+func main() {	
+	server :=http.NewServeMux()
+	
+	slog.Info("Starting the server on port 8080 ")
+
+	err := http.ListenAndServe(":8080", server)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		slog.Info("Server closed")
+	} else if err != nil {
+		slog.Error("Error starting server: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
