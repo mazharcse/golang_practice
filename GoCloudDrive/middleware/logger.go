f4281300dad3236050ae41b71c8ae09077108443
MD Mazharul Islam Khan <mazhar_cse@yahoo.com>
2025-05-20 18:00:50 +0600

    add custom logger

--- GoCloudDrive/middleware/logger.go
+++ GoCloudDrive/middleware/logger.go
@@ -6,11 +6,24 @@ import (
 	"time"
 )
 
+type RequestLogResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rlrw *RequestLogResponseWriter) WriteHeader(statusCode int) {
+	rlrw.statusCode = statusCode
+	rlrw.ResponseWriter.WriteHeader(statusCode)
+}
+
 func RequestLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request)  {
 		start := time.Now()
-		next.ServeHTTP(w, r)
 
-		fmt.Println("Cloud Drive", start.Format("2006-01-02 15:04:05"), r.Method, r.URL.Path, time.Since(start))
+		rlrw := &RequestLogResponseWriter{w, http.StatusOK,}
+
+		next.ServeHTTP(rlrw, r)
+
+		fmt.Println("Cloud Drive", start.Format("2006-01-02 15:04:05"), r.Method, rlrw.statusCode, r.URL.Path, time.Since(start))
 	})
 }
