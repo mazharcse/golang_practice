bce09d0301f17f0e9f7229dea3bd425218671f16
MD Mazharul Islam Khan <mazhar_cse@yahoo.com>
2025-05-21 15:15:46 +0600

    add configuration file (.env)

--- GoCloudDrive/main.go
+++ GoCloudDrive/main.go
@@ -8,11 +8,22 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
-)
 
+	"github.com/lpernett/godotenv"
+)
+  
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error("Error loading .env file" + err.Error())
+		os.Exit(1)
+	}
+	
+	port := os.Getenv("PORT")
+
 	server :=http.NewServeMux()
 
 	// server.HandleFunc("GET /hello", func(w http.ResponseWriter, r *http.Request) {
@@ -21,9 +32,9 @@ func main() {
  
 	server.HandleFunc("GET /hello", handler.Hello)
 	
-	slog.Info("Starting the server on port 8080 ")
+	slog.Info("Starting the server on port " + port + " ...")
 
-	err := http.ListenAndServe(":8080", middleware.RequestLogger(server))
+	err = http.ListenAndServe(":"+port, middleware.RequestLogger(server))
 
 	if errors.Is(err, http.ErrServerClosed) {
 		slog.Info("Server closed")
--- GoCloudDrive/middleware/logger.go
+++ GoCloudDrive/middleware/logger.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -24,6 +25,6 @@ func RequestLogger(next http.Handler) http.Handler {
 
 		next.ServeHTTP(rlrw, r)
 
-		fmt.Println("Cloud Drive", start.Format("2006-01-02 15:04:05"), r.Method, rlrw.statusCode, r.URL.Path, time.Since(start))
+		fmt.Println(os.Getenv("APP_NAME"), start.Format("2006-01-02 15:04:05"), r.Method, rlrw.statusCode, r.URL.Path, time.Since(start))
 	})
 }
